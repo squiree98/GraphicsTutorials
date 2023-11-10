3661116041445de0b8d7a2c33a4a71e91cccbaab
squire <e.d.squire1@newcastle.ac.uk>
2023-11-10 19:26:49 +0000

    Started courseowrk and implemented scene nodes with root node and had terrain be first scene node after root

--- Blank Project/Renderer.cpp
+++ Blank Project/Renderer.cpp
@@ -1,30 +1,126 @@
 #include "Renderer.h"
 
+#include "../nclgl/SceneNode.h"
+#include "../nclgl/Camera.h"
+#include "../nclgl/HeightMap.h"
+
+#include <algorithm>
+
+using namespace std;
+
 Renderer::Renderer(Window &parent) : OGLRenderer(parent)	{
-	triangle = Mesh::GenerateTriangle();
+	terrain = new HeightMap(TEXTUREDIR"noise.png");
 
-	basicShader = new Shader("basicVertex.glsl","colourFragment.glsl");
+	root = new SceneNode();
 
-	if(!basicShader->LoadSuccess()) {
+	Vector3 heightMapSize = terrain->GetHeightMapSize();
+	camera = new Camera(-45.0f, 0.0f, heightMapSize * Vector3(0.5f, 5.0f, 0.5f));
+
+	terrainTexture = SOIL_load_OGL_texture(TEXTUREDIR"Barren Reds.JPG", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS);
+	if(!terrainTexture)
 		return;
-	}
 
+	SetTextureRepeating(terrainTexture, true);
+
+	terrainShader = new Shader("TexturedVertex.glsl", "TexturedFragment.glsl");
+	if (!terrainShader->LoadSuccess())
+		return;
+
+	SceneNode* terrainNode = new SceneNode();
+	terrainNode->SetTransform(Matrix4::Translation(Vector3(0,0,0)));
+	terrainNode->SetMesh(terrain);
+	terrainNode->SetTexture(terrainTexture);
+
+	root->AddChild(terrainNode);
+
+	projMatrix = Matrix4::Perspective(1.0f, 15000.0f, (float)width / (float)height, 45.0f);
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
 	init = true;
 }
+
 Renderer::~Renderer(void)	{
-	delete triangle;
-	delete basicShader;
+	delete root;
+	delete terrain;
+	delete terrainShader;
+	delete camera;
 }
 
 void Renderer::UpdateScene(float dt) {
+	camera->UpdateCamera(dt);
+	viewMatrix = camera->BuildViewMatrix();
+	frameFrustrum.FromMatrix(projMatrix * viewMatrix);
 
+	root->Update(dt);
 }
 
 void Renderer::RenderScene()	{
-	glClearColor(0.2f,0.2f,0.2f,1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);	
+	BuildNodeLists(root);
+	SortNodeLists();
 
-	BindShader(basicShader);
-	triangle->Draw();
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+	BindShader(terrainShader);
+	UpdateShaderMatrices();
+
+	glUniform1i(glGetUniformLocation(terrainShader->GetProgram(), "diffuseTex"), 0);
+
+	DrawNodes();
+
+	ClearNodeLists();
 }
 
+void Renderer::BuildNodeLists(SceneNode* root) {
+	//if (frameFrustrum.InsideFrustrum(*root)) {
+		Vector3 dir = root->GetWorldTransform().GetPositionVector() - camera->GetPosition();
+		root->SetCameraDistance(Vector3::Dot(dir, dir));
+
+		if (root->GetColour().w < 1.0f)
+			transparentNodeList.push_back(root);
+		else
+			nodeList.push_back(root);
+
+		for (vector<SceneNode*>::const_iterator i = root->GetChildIteratorStart(); i != root->GetChildIteratorEnd(); i++) {
+			BuildNodeLists((*i));
+		}
+	//}
+}
+
+void Renderer::SortNodeLists() {
+	sort(transparentNodeList.rbegin(), transparentNodeList.rend(), SceneNode::CompareByCameraDistance);
+	sort(nodeList.rbegin(), nodeList.rend(), SceneNode::CompareByCameraDistance);
+}
+
+void Renderer::DrawNodes() {
+	for (const auto& i : nodeList)
+	{
+		DrawNode(i);
+	}
+	for (const auto& i : transparentNodeList)
+	{
+		DrawNode(i);
+	}
+}
+
+void Renderer::DrawNode(SceneNode* node) {
+	if (node->GetMesh()) {
+		Matrix4 model = node->GetWorldTransform() * Matrix4::Scale(node->GetModelScale());
+
+		glUniformMatrix4fv(glGetUniformLocation(terrainShader->GetProgram(), "modelMatrix"), 1, false, model.values);
+		glUniform4fv(glGetUniformLocation(terrainShader->GetProgram(), "nodeColour"), 1, (float*)&node->GetColour());
+
+		terrainTexture = node->GetTexture();
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, terrainTexture);
+
+		glUniform1i(glGetUniformLocation(terrainShader->GetProgram(), "useTexture"), terrainTexture);
+
+		node->Draw(*this);
+	}
+}
+
+void Renderer::ClearNodeLists() {
+	transparentNodeList.clear();
+	nodeList.clear();
+}
--- Blank Project/Renderer.h
+++ Blank Project/Renderer.h
@@ -1,13 +1,41 @@
 #pragma once
+
 #include "../NCLGL/OGLRenderer.h"
+#include "../nclgl/Frustrum.h"
+
+class Camera;
+class Shader;
+class HeightMap;
+class SceneNode;
 
 class Renderer : public OGLRenderer	{
 public:
 	Renderer(Window &parent);
 	 ~Renderer(void);
+
 	 void RenderScene()				override;
 	 void UpdateScene(float msec)	override;
+
 protected:
-	Mesh*	triangle;
-	Shader* basicShader;
+	// build node graph hierarchy
+	void BuildNodeLists(SceneNode* from);
+	void SortNodeLists();
+	void ClearNodeLists();
+	void DrawNodes();
+	void DrawNode(SceneNode* node);
+
+	SceneNode* root;
+
+	Shader*	   terrainShader;
+	
+	HeightMap* terrain;
+
+	Camera*	   camera;
+
+	GLuint	   terrainTexture;
+
+	//Frustrum frameFrustrum;
+
+	vector<SceneNode*> transparentNodeList;
+	vector<SceneNode*> nodeList;
 };
